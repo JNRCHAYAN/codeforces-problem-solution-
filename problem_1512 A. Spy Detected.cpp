b149fc23dbd857c95bc308f4b5192b08d2d40c87
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-10 12:23:55 +0600

    Solve 2 problme today

---
+++ problem_1512 A. Spy Detected.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,m,co=0;
+    cin >>n;
+    for(int i=0;i<n;i++)
+    {
+        cin >>m;
+        int arr[m];
+        for(int j=1;j<=m;j++)
+        {
+            cin >> arr[j];
+        }
+        for(int k=1;k<=m;k++)
+        {
+            if(arr[k] != arr[k+1] && arr[k+1] != arr[k+2])
+            {
+                co=k+1;
+                break;
+            }
+            else if(arr[k]!=arr[k+1] && arr[k+1]==arr[k+2])
+            {
+                co=k;
+                break;
+            }
+        }
+   
+        cout << co<< endl;
+        co=0;
+        
+    }
+}
---
+++ problem_1633_A Div 7.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,v,co=0;
+    cin >>n;
+    for(int i=0;i<n;i++)
+    {
+        cin >>v;
+       
+            if(v%7!=0)
+            {
+               co=v/7;
+               co=co*7;
+               if(co<10)
+               {
+                co=co*2;
+               }
+               cout<< co << endl; 
+            }
+            else
+            {
+                cout <<v<< endl;
+            }
+        
+    }
+}
